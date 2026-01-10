74fcef2a3ce6b031c49f1728e205128fefd99995
浪小矢 <djchan411130@gmail.com>
2026-01-10 23:18:21 +0800

    fix: 调整直接下载限制，以适应Cloudflare Worker限制
    由于Cloudflare Worker免费版有128MB内存限制，将DIRECT_DOWNLOAD_LIMIT从20个分片调整为2个分片，避免大文件下载后内存不足报错

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -93,6 +93,10 @@ export type ListFilesResponse = {
 // === 常量 ===
 export const MAX_CONCURRENTS = 3  // 最大并发上传数
 export const MAX_CHUNK_SIZE = 20 * 1024 * 1024 // 20MB
-export const DIRECT_DOWNLOAD_LIMIT = MAX_CHUNK_SIZE * 20 // 小的媒体文件可通过a.click或fetchBlob直接下载，超过则让用户通过浏览器控件下载
+
+// Cloudflare Worker免费版限制: https://developers.cloudflare.com/workers/platform/limits/#worker-limits
+// 最大128MB内存, 因此在下载大文件后，再次下载可能会报错
+export const DIRECT_DOWNLOAD_LIMIT = MAX_CHUNK_SIZE * 2 // 小的媒体文件可通过a.click直接下载，超过则让用户通过浏览器控件下载
 export const MAX_CHUNK_NUM = 50 // 由于Cloudflare Worker的CPU限制，这里限制最大分片数为50, 即文件大小不得超过1000MB≈1GB
 export const MAX_FILE_SIZE = MAX_CHUNK_SIZE * MAX_CHUNK_NUM
+
